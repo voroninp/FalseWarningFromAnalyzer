aae2add24038b17be37124d7545ac67842855727
Pavel Voronin <pavel@mediabuyingsystems.com>
2024-07-18 18:46:23 +0200

    Add project files.

---
+++ LoggerExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.Extensions.Logging;
+
+namespace FalseWarningFromAnalyzer;
+
+public static partial class LoggerExtensions
+{
+    [LoggerMessage(
+            Level = LogLevel.Warning,
+            Message = "We are using custom SQL. This is not stable against migrations. Method: {Method} Explanation: {Explanation}")]
+    public static partial void WarnCustomSqlIsUsed(this ILogger logger, string explanation, [CallerMemberName] string? method = null);
+}
